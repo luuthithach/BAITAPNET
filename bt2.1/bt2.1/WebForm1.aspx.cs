3cba5f337ec96f36d4520e3824fe3e1dadca27ac
luuthithach <52956922+luuthithach@users.noreply.github.com>
2019-08-16 15:51:35 +0700

    Add files via upload

---
+++ bt2.1/bt2.1/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bt2._1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid && FileUpload1.HasFile)
+            {
+                // string fileName = "images/" + FileUpload1.FileName;
+                string fileName = FileUpload1.FileName;
+                string filePath = MapPath(fileName);
+                FileUpload1.SaveAs(filePath);
+                Image1.ImageUrl = fileName;
+            }
+        }
+    }
+}
